7d6067d5e6102a2dcd22043d22f3dfd8cbe72e05
Peter Georgeson <peter@supernifty.org>
2022-05-12 16:23:16 +1000

    scripts to generate data

--- util/generate_static.py
+++ util/generate_static.py
@@ -11,19 +11,30 @@ import sys
 
 # #bin	name	chrom	strand	txStart	txEnd	cdsStart	cdsEnd	exonCount	exonStarts	exonEnds	score	name2	cdsStartStat	cdsEndStat	exonFrames
 #0	NM_032291	chr1	+	66999638	67216822	67000041	67208778	25	66999638,67091529,67098752,67101626,67105459,67108492,67109226,67126195,67133212,67136677,67137626,67138963,67142686,67145360,67147551,67154830,67155872,67161116,67184976,67194946,67199430,67205017,67206340,67206954,67208755,	67000051,67091593,67098777,67101698,67105516,67108547,67109402,67126207,67133224,67136702,67137678,67139049,67142779,67145435,67148052,67154958,67155999,67161176,67185088,67195102,67199563,67205220,67206405,67207119,67216822,	0	SGIP1	cmpl	cmpl	0,1,2,0,0,0,1,0,0,0,1,2,1,1,1,1,0,1,1,2,2,0,2,1,1,
-def main(tx, ofh):
+def main(tx, canonical_fn, ofh):
   logging.info('starting...')
+
+  canonical=set()
+  for r in open(canonical_fn, 'rt'):
+    canonical.add(r.strip().split('.')[0])
+
+  logging.info('%i canonical transcripts', len(canonical))
   
   odw = csv.DictWriter(ofh, delimiter='\t', fieldnames=['gene', 'tx', 'chrom', 'txstart', 'txend', 'genome'])
   odw.writeheader()
   for r in csv.DictReader(gzip.open(tx, 'rt'), delimiter='\t'):
-    odw.writerow({'gene': r['name2'], 'chrom': r['chrom'], 'tx': r['name'], 'txstart': r['txStart'], 'txend': r['txEnd'], 'genome': 'GRCh37'})
+    if r['name'] in canonical:
+      tx_name = '{} canonical'.format(r['name'])
+    else:
+      tx_name = r['name']
+    odw.writerow({'gene': r['name2'], 'chrom': r['chrom'], 'tx': tx_name, 'txstart': r['txStart'], 'txend': r['txEnd'], 'genome': 'GRCh37'})
 
   logging.info('done')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='data for genome helper')
-  parser.add_argument('--tx', required=True, help='tumour vcf')
+  parser.add_argument('--tx', required=True, help='transcripts')
+  parser.add_argument('--canonical', required=True, help='canonical transcripts')
   parser.add_argument('--verbose', action='store_true', help='more logging')
   args = parser.parse_args()
   if args.verbose:
@@ -31,4 +42,4 @@ if __name__ == '__main__':
   else:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
-  main(args.tx, sys.stdout)
+  main(args.tx, args.canonical, sys.stdout)
---
+++ util/generate_static.sh
@@ -0,0 +1,2 @@
+
+util/generate_static.py --tx data/refseq.ucsc.hg19.180829.gz --canonical data/canonical.txt > static/tx.tsv
